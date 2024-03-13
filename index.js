6854772a269650da7fc6246f5827ccc80f483692
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-03-13 23:49:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,30 +10,27 @@ function addElmToMenu (food){
                 <p class="ingredients">Ingredients: ${foodElm.ingredients}</p>
                 <p class="price">${foodElm.price}$</p>
             </div>
-            <button class="addBtn" id="${foodElm.id}">+</button>
+            <button class="addBtn" data-id="${foodElm.id}">+</button>
         </div>`
     })
 }
 
 foodList.innerHTML = addElmToMenu(menuArray).join('')
 
-const menuItem = menuArray.map(function(foodId){
-    return foodId
-})
-
 
 document.addEventListener('click', function(e) {
-    if (e.target.id){
-        addingOrderedFood(e.target.id)
+    if (e.target.dataset.id){
+        addingOrderedFood(e.target.dataset.id)
+        //console.log(e.target.dataset.id)
     }
 
 })
 
-function addingOrderedFood (food){
-    const addItem = menuArray.filter(function(foodObj){
-        return foodObj.id === food
-        })[0]
-
-    console.log (addItem)
-
+function addingOrderedFood (foodXx){
+    const foodToAdd = menuArray.filter(function(foodObj){
+        //console.log(typeof foodObj.id)
+        //console.log(typeof Number(foodXx))
+        return foodObj.id == Number(foodXx)
+    })[0]
+    console.log(foodToAdd)
 }
